660fb4e2546f58d27327995958dd903e359fc02d
Mohammadreza <engineer.it93@gmail.com>
2022-09-26 01:55:07 +0330

    Write apis test

--- admediatorsdk/src/test/java/com/mra/admediatorsdk/ApisTest.kt
+++ admediatorsdk/src/test/java/com/mra/admediatorsdk/ApisTest.kt
@@ -1,35 +1,26 @@
 package com.mra.admediatorsdk
 
 
-import android.util.Log
 import com.google.gson.GsonBuilder
-import com.mra.admediatorsdk.core.base.CustomResult
 import com.mra.admediatorsdk.core.di.createBaseNetworkClient
 import com.mra.admediatorsdk.data.remote.api.AdApiService
-import com.mra.admediatorsdk.data.repo.AdRepo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
-import org.koin.java.KoinJavaComponent.inject
 import java.util.concurrent.TimeUnit
 
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Create by Mohammadreza Allahgholi
+ *  Site: https://seniorandroid.ir
  */
 class ApisTest {
 
-    private lateinit var api : AdApiService
+    private lateinit var api: AdApiService
 
     @BeforeEach
-    fun initApiTest(){
-       api = createBaseNetworkClient(GsonBuilder().create(), BuildConfig.TAPSELL_BASE_URL).create(
+    fun initApiTest() {
+        api = createBaseNetworkClient(GsonBuilder().create(), BuildConfig.TAPSELL_BASE_URL).create(
             AdApiService::class.java
         )
     }
